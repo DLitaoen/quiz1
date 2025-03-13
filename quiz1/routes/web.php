a070c8344c620baa8f8933d760aeecfc81378684
Alby Destiny Litaoen <144974382+DLitaoen@users.noreply.github.com>
2025-03-13 00:20:07 +0000

    Add route for hello/{name}

--- quiz1/routes/web.php
+++ quiz1/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// This tells the website to listen for a link like "hello/John" and grab the "John" part
+Route::get('hello/{name}', function($name) {
+    return 'hi ' . $name; // It will say "hi" followed by whatever name you typed (like "hi John")
+});
