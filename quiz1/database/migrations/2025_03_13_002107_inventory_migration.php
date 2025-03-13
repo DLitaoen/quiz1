3d0dd264ce20b7b871adc032322c52d56d69417c
Alby Destiny Litaoen <144974382+DLitaoen@users.noreply.github.com>
2025-03-13 01:22:04 +0000

    Clean code for InventoryMigration

--- quiz1/database/migrations/2025_03_13_002107_inventory_migration.php
+++ quiz1/database/migrations/2025_03_13_002107_inventory_migration.php
@@ -6,30 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         // Create the table
         Schema::create('inventory', function (Blueprint $table) {
-
             $table->id();                   // Primary key (auto-increment)
-
             $table->string('name');         // Item name
-
             $table->integer('quantity');    // Quantity in stock
-
             $table->decimal('price', 8, 2); // Price per item
-
             $table->timestamps();           // Created at and updated at
-
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         // Drop the table
